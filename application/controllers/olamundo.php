3351a984be252e41336c03b7a8f9a884d8f4a11e
MateusOT <mateustrevisan275@gmail.com>
2015-07-22 10:27:10 -0400

    test

---
+++ application/controllers/olamundo.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Olamundo extends CI_Controller{
+
+    public  function index(){
+        echo __FUNCTION__;
+    }
+    public function incluir(){
+
+    }
+}
